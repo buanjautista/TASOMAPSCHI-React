ab6c10bf8b9490aef5bc6fee35cfc9dd4849b3c1
bauti777 <bauti777@gmail.com>
2023-11-04 02:27:09 -0300

    fix: rename classes and components

--- src/components/MapFrame.jsx
+++ src/components/MapFrame.jsx
@@ -6,9 +6,11 @@ export const MapFrame = ({lampState, currentArea}) => {
   let mapImage = (getAreaInfo(currentArea).mapImage) || "./src/assets/images/ShiokazePier.png"
   return (
     <div>
+      <div className="lampBox">
       {
         lampState?.map((name) => {return (<div>{name.name}</div>)})
       }
+      </div>
       <img src={mapImage} className="map-img" />
     </div>
   )
--- src/components/SwitchBox.jsx
+++ src/components/SwitchBox.jsx
@@ -10,9 +10,9 @@ export const SwitchBox = ({setLamps, lampState, currentArea}) => {
   const LampSwitch = ({lamp, index}) => {
       return (
         <>
-          <button onClick={setLamps} className={`lamp-button ${lampState[index] && "lamp-active"}`} value={index}>
+          <button onClick={setLamps} className={`lamp-button ${lampState[index] && "switch-active"}`} value={index}>
             {lamp.name}
-            <div className="lamp-extra-info">
+            <div className="switch-extra-info">
             {(lamp.sourcereq > 0) && <span className="source-req">{lamp.sourcereq}</span>}
           { (lamp.airship == ("true" || "upgraded")) && <img src="./src/assets/images/airship.png" className={`icon ${(lamp.airship == "upgraded") && "upgraded"}`}/> }
             </div>
@@ -22,7 +22,7 @@ export const SwitchBox = ({setLamps, lampState, currentArea}) => {
   }
 
   return (
-    <form onSubmit={setLamps} className="lamp-grid">
+    <form onSubmit={setLamps} className="switch-grid">
       {
         currentAreaLamps.map((lamp,index) => {
           return (
